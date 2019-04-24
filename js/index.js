7f1c0ebc6c44e710da8431c217f95975434d7208
ghankeliu2015 <hanke.liu@gmail.com>
2019-04-23 21:39:48 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+// append element to body to start:
+let element = document.createElement('div')
+element.innerHTML = 'Helllo, DOM!';
+element.style.backgroundColor = "#f9f9f9";
+document.body.appendChild(element);
+element.style.textAlign = 'center';
+
+// append elements to that element:
+let ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+// element.appendChild(ul);
+ul.style.textAlign = 'left';
+Remove element from DOM
+ul.removeChild(ul.querySelector('li:nth-child(2)'));	//remove li
+ul.remove();		//remove entirel ul
